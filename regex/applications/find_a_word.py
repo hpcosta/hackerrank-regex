64efbca330ea4417cba96b1a5872ab9a0a754020
Hernani Costa <hpcosta@uma.es>
2016-12-16 11:59:14 +0100

    find a word

--- regex/applications/find_a_word.py
+++ regex/applications/find_a_word.py
@@ -6,3 +6,21 @@ Created on 15 Dec 2016
 
 # https://www.hackerrank.com/challenges/find-a-word
 
+import re
+
+# number of lines
+n_lines = int(raw_input())
+
+
+content = []
+for i in range(0, n_lines):
+   content.append(raw_input())
+# content
+str_content = ' '.join(content)
+
+
+# number of test cases
+n_test_cases = int(raw_input())
+for j in range(0, n_test_cases):
+   sub_word = raw_input().strip()
+   print(len(re.findall(r'\b%s\b' % sub_word, str_content)))
